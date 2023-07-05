11dc767c424ded1958256786eb14c71525924708
LukiyanK <aseev.kostya@gmail.com>
2023-07-05 17:43:12 +0300

    new dir

--- Kafka_stream_faust/kafka_faust.py
+++ Kafka_stream_faust/kafka_faust.py
@@ -12,3 +12,16 @@ async def order(orders):
     async for order in orders:
         # process infinite stream of orders.
         print(f'Order for {order.account_id}: {order.amount}')
+
+
+# data sent to 'clicks' topic sharded by URL key.
+# e.g. key="http://example.com" value="1"
+click_topic = app.topic('clicks', key_type=str, value_type=int)
+
+# default value for missing URL will be 0 with `default=int`
+counts = app.Table('click_counts', default=int)
+
+@app.agent(click_topic)
+async def count_click(clicks):
+    async for url, count in clicks.items():
+        counts[url] += count
